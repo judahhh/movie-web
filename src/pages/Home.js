eee9312340f43f5f6e9d4f5c41237df65ed294e7
judahhh <jdh99080516@gmail.com>
2023-01-28 15:28:44 +0900

    feat : 전역 데이터 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,40 @@
 import React from "react";
 import { useEffect } from "react";
 import { movieAction } from "../redux/actions/movieAction";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Banner from "../components/Banner";
+import MovieSlide from "../components/MovieSlide";
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const { popularMovies, topRatedMovies, upComingMovies, loading } =
+    useSelector((state) => state.movie);
+  console.log("home", popularMovies);
 
   useEffect(() => {
     dispatch(movieAction.getMovies());
-  }, []);
-  return <div>Home</div>;
+  }, []); //렌더를 하고 호출이 되기 때문에 popularMovies를 모름
+
+  //로딩이 true면 로딩스피너 보여주고 로딩이 false면 데이터 보여줌
+  if (loading) {
+    return <ClipLoader color="#ffffff" loading={loading} size={150} />;
+  }
+  return (
+    <div>
+      {/* 이부분에서 조건부 렌더링 필요 */}
+
+      <Banner movie={popularMovies.results[1]} />
+
+      <h1>Popular Movies</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>Top rated Movies</h1>
+      <MovieSlide movies={topRatedMovies} />
+      <h1>Upcoming Movies</h1>
+      <MovieSlide movies={upComingMovies} />
+    </div>
+  );
 };
 
 export default Home;
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -3,38 +3,50 @@ import api from "../api";
 const API_KEY = process.env.REACT_APP_API_KEY;
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = await api.get(
-      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    );
+    try {
+      dispatch({ type: "GET_MOVIES_REQUEST" });
+      const popularMovieApi = await api.get(
+        `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
 
-    const topRatedApi = await api.get(
-      `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-    );
-    const upComingApi = await api.get(
-      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    );
+      const topRatedApi = await api.get(
+        `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      const upComingApi = await api.get(
+        `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      const genreApi = await api.get(
+        `/genre/movie/list?api_key=${API_KEY}&language=en-US`
+      );
 
-    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
-      popularMovieApi,
-      topRatedApi,
-      upComingApi,
-    ]);
+      let [popularMovies, topRatedMovies, upComingMovies, genreList] =
+        await Promise.all([
+          popularMovieApi,
+          topRatedApi,
+          upComingApi,
+          genreApi,
+        ]);
+      console.log("장르 리스트", genreList);
 
-    dispatch({
-      type: "GET_MOVIES_SUCCESS",
-      payload: {
-        popularMovies: popularMovies.data,
-        topRatedMovies: topRatedMovies.data,
-        upComingMovies: upComingMovies.data,
-      },
-    });
+      dispatch({
+        type: "GET_MOVIES_SUCCESS",
+        payload: {
+          popularMovies: popularMovies.data,
+          topRatedMovies: topRatedMovies.data,
+          upComingMovies: upComingMovies.data,
+          genreList: genreList.data.genres,
+        },
+      });
 
-    console.log(
-      "Promise all 후에 받는 데이터 : ",
-      popularMovies,
-      topRatedMovies,
-      upComingMovies
-    );
+      console.log(
+        "Promise all 후에 받는 데이터 : ",
+        popularMovies,
+        topRatedMovies,
+        upComingMovies
+      );
+    } catch (err) {
+      dispatch({ type: "GET_MOVIES_FAILURE" });
+    }
   };
 }
 
